7e5b36bc7a9de050314b07f2593b030e37bc8676
Freddy <frederic.charon@gmx.de>
2022-10-06 13:50:28 +0200

    connecting repo

---
+++ modules/dataset.py
@@ -0,0 +1,60 @@
+import os
+
+import torch
+from torch.utils.data import Dataset
+from skimage import io
+
+from utils import generate_phoc_vector, generate_phos_vector
+
+import pandas as pd
+import numpy as np
+
+
+class phosc_dataset(Dataset):
+    def __init__(self, csvfile, root_dir, transform=None, calc_phosc=True):
+        self.root_dir = root_dir
+        self.transform = transform
+        self.df_all = pd.read_csv(csvfile, usecols=["Image", "Word"])
+        self.df_all["phos"] = self.df_all['Word'].apply(generate_phos_vector)
+        self.df_all["phoc"] = self.df_all['Word'].apply(generate_phoc_vector)
+
+        if calc_phosc:
+            self.df_all['phosc'] = self.df_all['phos']
+            for i in range(len(self.df_all["phos"])):
+                self.df_all['phosc'][i] = np.concatenate((self.df_all["phos"][i], self.df_all["phoc"][i]))
+
+        print(len(self.df_all['phosc'][0]))
+        print(self.df_all)
+        # len phos-vector: 165
+        # len phoc-vector: 604
+
+        # Fill in your code here. You will populate self.df_all
+        # it should be pandas df with ["Image", "Word", "phos", "phoc", "phosc"] columns
+        # containing file name, word label, phoc, phoc, phosc features vector in each row
+        # phosc features vector can be created combining generate_phos_vector, generate_phoc_vector
+        # Note: How to use phoc, phoc or phosc of the df in a batch is up to you.
+        # in the __getitem__ below the phosc vector is used in the batches.
+
+    def __getitem__(self, index):
+        img_path = os.path.join(self.root_dir, self.df_all.iloc[index, 0])
+        image = io.imread(img_path)
+
+        y = torch.tensor(self.df_all.iloc[index, len(self.df_all.columns) - 1])
+
+        if self.transform:
+            image = self.transform(image)
+
+        return image.float(), y.float(), self.df_all.iloc[index, 1]
+
+    def __len__(self):
+        return len(self.df_all)
+
+
+if __name__ == '__main__':
+    from torchvision.transforms import transforms
+
+    dataset = phosc_dataset('image_data/IAM_test_unseen.csv', '../image_data/IAM_test', transform=transforms.ToTensor())
+
+    print(dataset.df_all)
+
+    print(dataset.__getitem__(0))
---
+++ modules/loss.py
@@ -0,0 +1,32 @@
+import torch
+
+import torch.nn as nn
+from torch.nn import functional as F
+
+
+class PHOSCLoss(nn.Module):
+    def __init__(self, phos_w=4.5, phoc_w=1):
+        super().__init__()
+
+        self.phos_w = phos_w
+        self.phoc_w = phoc_w
+
+    def forward(self, y: dict, targets: torch.Tensor):
+
+        #print(len(targets))
+        # Apply the loss on PHOS features this is a regression loss
+        # Note: This loss should be applicable to the PHOS part of the
+        # output which is the first part of the output.
+        phos_loss = self.phos_w * F.mse_loss(y['phos'], targets[:, :165])
+
+        #for i in y['phoc']:
+           # print(i)
+
+        #aloss = nn.functional.mse_loss(y['phoc'], targets)
+        # Apply the loss on PHOC features this is a classification loss
+        # Note: This loss should be applicable to the PHOC part of the
+        # output which is the later part of the output.
+        phoc_loss = self.phoc_w * F.binary_cross_entropy(y['phoc'], targets[:, 165:])
+
+        loss = phos_loss + phoc_loss
+        return loss
